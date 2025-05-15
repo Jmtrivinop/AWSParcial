c02ca63d7ffcde8e2dff56a9819d04b88741567f
Jmtrivinop <131928328+Jmtrivinop@users.noreply.github.com>
2025-05-15 11:30:38 -0500

    Primer commit

---
+++ MoneyBankService/MoneyBankService.Application/Dto/AccountDto.cs
@@ -0,0 +1,5 @@
+﻿namespace MoneyBankService.Application.Dto;
+
+public class AccountDto
+{
+}
---
+++ MoneyBankService/MoneyBankService.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MoneyBankService.Application.Dto;
+using MoneyBankService.Domain.Entities;
+
+namespace MoneyBankService.Application.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<AccountDto, Account>();
+        CreateMap<Account, AccountDto>();
+
+    }
+}
